8a69795a0613048439436b98ff1c22f1fbd50246
NicoEstebanBorquez <nico.esteban.borquez@gmail.com>
2022-03-18 15:00:09 +0100

    First commit

---
+++ src/main/java/com/plenium/proyecto/PleniumApplication.java
@@ -0,0 +1,13 @@
+package com.plenium.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PleniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plenium/proyecto/PleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plenium.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
